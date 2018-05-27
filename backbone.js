a2a85125f2d6d7976afb7c1fa0412d1564fe6ccc
Luiz Américo <luizamericop@gmail.com>
2018-05-27 09:34:38 -0300

    Remove noConflict

--- backbone.js
+++ backbone.js
@@ -35,10 +35,6 @@
   // Initial Setup
   // -------------
 
-  // Save the previous value of the `Backbone` variable, so that it can be
-  // restored later on, if `noConflict` is used.
-  var previousBackbone = root.Backbone;
-
   // Create a local reference to a common array method we'll want to use later.
   var slice = Array.prototype.slice;
 
@@ -50,14 +46,6 @@
 
   // Current version of the library. Keep in sync with `package.json`.
   Backbone.VERSION = '1.3.3';
-
-  // Runs Backbone.js in *noConflict* mode, returning the `Backbone` variable
-  // to its previous owner. Returns a reference to this Backbone object.
-  Backbone.noConflict = function() {
-    root.Backbone = previousBackbone;
-    return this;
-  };
-
 
 
   // Backbone.Events
