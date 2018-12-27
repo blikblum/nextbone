ce3d38f3a0e3afb2c783a20a35802c83a6e9e796
Luiz Américo <luizamericop@gmail.com>
2018-12-26 23:38:14 -0200

    Initial import of localstorage

---
+++ localstorage.js
@@ -0,0 +1,283 @@
+import { chain, contains, isObject, without, isUndefined } from 'underscore';
+
+/** Generates 4 random hex digits
+ * @returns {string} 4 Random hex digits
+*/
+function s4() {
+  const rand = (1 + Math.random()) * 0x10000;
+  return (rand | 0).toString(16).substring(1);
+}
+
+/** Generate a pseudo-guid
+ * @returns {string} A GUID-like string.
+ */
+export function guid() {
+  return `${s4()}${s4()}-${s4()}-${s4()}-${s4()}-${s4()}${s4()}${s4()}`;
+}
+
+/** The default serializer for transforming your saved data to localStorage */
+const defaultSerializer = {
+
+  /** Return a JSON-serialized string representation of item
+   * @param {Object} item - The encoded model data
+   * @returns {string} A JSON-encoded string
+   */
+  serialize(item) {
+    return isObject(item) ? JSON.stringify(item) : item;
+  },
+
+  /** Custom deserialization for data.
+   * @param {string} data - JSON-encoded string
+   * @returns {Object} The object result of parsing data
+   */
+  deserialize(data) {
+    return JSON.parse(data);
+  }
+};
+
+/** LocalStorage proxy class for Backbone models.
+ * Usage:
+ *   export const MyModel = Backbone.Model.extend({
+ *     localStorage: new LocalStorage('MyModelName')
+ *   });
+ */
+class LocalStorage {
+  constructor(name = '', serializer = defaultSerializer) {
+    this.name = name;
+    this.serializer = serializer;
+
+    const store = this._getItem(this.name);
+    this.records = (store && store.split(',')) || [];
+  }
+
+  /** Return the global localStorage variable
+   * @returns {Object} Local Storage reference.
+  */
+  localStorage() {
+    return window.localStorage;
+  }
+
+  /** Save the current status to localStorage
+   * @returns {undefined}
+   */
+  save() {
+    this._setItem(this.name, this.records.join(','));
+  }
+
+  /** Add a new model with a unique GUID, if it doesn't already have its own ID
+   * @param {Model} model - The Backbone Model to save to LocalStorage
+   * @returns {Model} The saved model
+   */
+  create(model) {
+    if (!model.id && model.id !== 0) {
+      model.id = guid();
+      model.set(model.idAttribute, model.id);
+    }
+
+    this._setItem(this._itemName(model.id), this.serializer.serialize(model));
+    this.records.push(model.id.toString());
+    this.save();
+
+    return this.find(model);
+  }
+
+  /** Update an existing model in LocalStorage
+   * @param {Model} model - The model to update
+   * @returns {Model} The updated model
+   */
+  update(model) {
+    this._setItem(this._itemName(model.id), this.serializer.serialize(model));
+
+    const modelId = model.id.toString();
+
+    if (!contains(this.records, modelId)) {
+      this.records.push(modelId);
+      this.save();
+    }
+    return this.find(model);
+  }
+
+  /** Retrieve a model from local storage by model id
+   * @param {Model} model - The Backbone Model to lookup
+   * @returns {Model} The model from LocalStorage
+   */
+  find(model) {
+    return this.serializer.deserialize(this._getItem(this._itemName(model.id)));
+  }
+
+  /** Return all models from LocalStorage
+   * @returns {Array} The array of models stored
+   */
+  findAll() {
+    return chain(this.records).map(
+      id => this.serializer.deserialize(this._getItem(this._itemName(id)))
+    ).filter(
+      item => item != null).value();
+  }
+
+  /** Delete a model from `this.data`, returning it.
+   * @param {Model} model - Model to delete
+   * @returns {Model} Model removed from this.data
+  */
+  destroy(model) {
+    this._removeItem(this._itemName(model.id));
+    const newRecords = without(this.records, model);
+
+    this.records = newRecords;
+    this.save();
+
+    return model;
+  }
+
+  /** Clear localStorage for a collection
+   * @returns {undefined}
+   */
+  _clear() {
+    const local = window.localStorage;
+    const itemRe = new RegExp(`^${this.name}-`);
+
+    // Remove id-tracking item (e.g., "foo").
+    local.removeItem(this.name);
+
+    // Match all data items (e.g., "foo-ID") and remove.
+    for (let k in local) {
+      if (itemRe.test(k)) {
+        local.removeItem(k);
+      }
+    }
+
+    this.records.length = 0;
+  }
+
+  /** Number of items in localStorage
+   * @returns {integer} - Number of items
+   */
+  _storageSize() {
+    return window.localStorage.length;
+  }
+
+  /** Return the item from localStorage
+   * @param {string} name - Name to lookup
+   * @returns {string} Value from localStorage
+   */
+  _getItem(name) {
+    return window.localStorage.getItem(name);
+  }
+
+  /** Return the item name to lookup in localStorage
+   * @param {integer} id - Item ID
+   * @returns {string} Item name
+  */
+  _itemName(id) {
+    return `${this.name}-${id}`;
+  }
+
+  /** Proxy to the localStorage setItem value method
+   * @param {string} key - LocalStorage key to set
+   * @param {string} value - LocalStorage value to set
+   * @returns {undefined}
+   */
+  _setItem(key, value) {
+    window.localStorage.setItem(key, value);
+  }
+
+  /** Proxy to the localStorage removeItem method
+   * @param {string} key - LocalStorage key to remove
+   * @returns {undefined}
+   */
+  _removeItem(key) {
+    window.localStorage.removeItem(key);
+  }
+};
+
+/** Returns the localStorage attribute for a model
+ * @param {Model} model - Model to get localStorage
+ * @returns {Storage} The localstorage
+ */
+function getLocalStorage(model) {
+  return model.localStorage || (model.collection && model.collection.localStorage);
+}
+
+/** Override Backbone's `sync` method to run against localStorage
+ * @param {string} method - One of read/create/update/delete
+ * @param {Model} model - Backbone model to sync
+ * @param {Object} options - Options object, use `ajaxSync: true` to run the
+ *  operation against the server in which case, options will also be passed into
+ *  `jQuery.ajax`
+ * @returns {undefined}
+ */
+function sync(method, model, options) {
+  const store = getLocalStorage(model);
+  let resp, errorMessage;
+  let resolve, reject;
+  const promise = new Promise((res, rej) => {
+    resolve = res;
+    reject = rej;
+  });
+
+  try {
+    switch (method) {
+      case 'read':
+        resp = isUndefined(model.id) ? store.findAll() : store.find(model);
+        break;
+      case 'create':
+        resp = store.create(model);
+        break;
+      case 'patch':
+      case 'update':
+        resp = store.update(model);
+        break;
+      case 'delete':
+        resp = store.destroy(model);
+        break;
+    }
+
+  } catch (error) {
+    if (error.code === 22 && store._storageSize() === 0) {
+      errorMessage = 'Private browsing is unsupported';
+    } else {
+      errorMessage = error.message;
+    }
+  }
+
+  if (resp) {
+    if (options.success) {
+      options.success.call(model, resp, options);
+    }
+    resolve(resp);
+  } else {
+    errorMessage = errorMessage ? errorMessage : 'Record Not Found';
+
+    if (options.error) {
+      options.error.call(model, errorMessage, options);
+    }
+    reject(errorMessage);
+  }
+
+  // add compatibility with $.ajax
+  // always execute callback for success and error
+  if (options.complete) {
+    options.complete.call(model, resp);
+  }
+
+  return promise;
+}
+
+export function localStorage(name, serializer) {
+  return ModelClass => {
+    return class extends ModelClass {
+      constructor(...args) {
+        super(...args);
+        this.localStorage = new LocalStorage(name, serializer);
+      }
+
+      sync(method, model, options = {}) {
+        const forceAjaxSync = options.ajaxSync;
+        const hasLocalStorage = getLocalStorage(model);
+
+        const fn = !forceAjaxSync && hasLocalStorage ? sync : super.sync;
+        fn(method, model, options);
+      }
+    }
+  }
+}
