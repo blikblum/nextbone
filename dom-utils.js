9f66f00de77ff07ea9d921f9c8a8d671afc8cb83
Luiz Américo <luizamericop@gmail.com>
2020-02-01 10:01:46 -0300

    dom-utils: use existing child element as current el in Region

--- dom-utils.js
+++ dom-utils.js
@@ -1,35 +1,35 @@
 export class Region {
-  constructor (targetEl) {
-    this.targetEl = targetEl
-    this.isSwappingEl = false
-    this.currentEl = undefined
+  constructor(targetEl, currentEl = targetEl.firstElementChild) {
+    this.targetEl = targetEl;
+    this.isSwappingEl = false;
+    this.currentEl = currentEl;
   }
 
-  show (el) {
-    this.isSwappingEl = this.currentEl !== undefined
+  show(el) {
+    this.isSwappingEl = this.currentEl != null;
 
     if (this.currentEl) {
-      this.empty()
+      this.empty();
     }
 
-    this.attachEl(el)
+    this.attachEl(el);
 
-    this.currentEl = el
-    this.isSwappingEl = false
+    this.currentEl = el;
+    this.isSwappingEl = false;
   }
 
-  empty () {
+  empty() {
     if (this.currentEl) {
-      this.detachEl(this.currentEl)
+      this.detachEl(this.currentEl);
     }
-    this.currentEl = undefined
+    this.currentEl = undefined;
   }
 
-  attachEl (el) {
-    this.targetEl.appendChild(el)
+  attachEl(el) {
+    this.targetEl.appendChild(el);
   }
 
-  detachEl (el) {
-    this.targetEl.removeChild(el)
+  detachEl(el) {
+    this.targetEl.removeChild(el);
   }
 }
