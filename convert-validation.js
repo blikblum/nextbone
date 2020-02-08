d42fe99be1dddfefaba6f7476c73e96280059135
Luiz Américo <luizamericop@gmail.com>
2020-02-07 22:48:56 -0300

    add validation syntax jscodeshift transformer

---
+++ convert-validation.js
@@ -0,0 +1,21 @@
+module.exports = function transformer(file, api) {
+  const j = api.jscodeshift;
+  const root = j(file.source);
+
+  root.find(j.ClassDeclaration).forEach(path => {
+    const decorator =
+      path.value.decorators &&
+      path.value.decorators.find(decorator => {
+        return decorator.expression.callee.name === 'validation';
+      });
+    if (decorator) {
+      const options = decorator.expression.arguments[0];
+      const classProperty = j.classProperty(j.identifier('validation'), options, null, true);
+      path.value.body.body = [classProperty, ...path.value.body.body];
+      path.value.decorators = [];
+    }
+  });
+  return root.toSource();
+};
+
+module.exports.parser = 'babylon';
